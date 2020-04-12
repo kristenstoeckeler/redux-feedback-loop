ad3939d7b54386eee88bfaadbc839bba54a7bee5
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-04-11 19:01:11 -0500

    thank you page allows user to take another survey & empties out redux state on handleSubmit

--- src/components/Thanks/Thanks.js
+++ src/components/Thanks/Thanks.js
@@ -4,10 +4,30 @@ import { withStyles } from '@material-ui/core/styles';
 
 
 class Thanks extends Component {
+    state = {
+        feeling: 0,
+        understanding: 0,
+        support: 0,
+        comments: '',
+    }
+
+    handleSubmit = (event) => {
+        console.log('In handleSubmit');
+        event.preventDefault();
+        this.props.dispatch({
+            type: 'FEEDBACK',
+            payload: this.state
+        });
+        this.props.history.push('/');
+    }
+
     render() {
         return (
             <>
-                <h5>Thank you for leaving a review!</h5>
+                <h5>Success! Thank you for leaving a review.</h5>
+                <form onSubmit={this.handleSubmit}>
+                    <button type="submit">Take Another Survey</button>
+                </form>
             </>
         );
     }
